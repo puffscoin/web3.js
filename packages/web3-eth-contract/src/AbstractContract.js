13c7ca660333750c22d6a7f696ce4f53a219efda
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-20 21:45:25 -0300

    Update AbstractContract.js

--- packages/web3-eth-contract/src/AbstractContract.js
+++ packages/web3-eth-contract/src/AbstractContract.js
@@ -24,7 +24,7 @@ import {AbstractWeb3Module} from 'web3-core';
 
 export default class AbstractContract extends AbstractWeb3Module {
     /**
-     * @param {Web3EthereumProvider|HttpProvider|WebsocketProvider|IpcProvider|String} provider
+     * @param {Web3PuffscoinProvider|HttpProvider|WebsocketProvider|IpcProvider|String} provider
      * @param {ContractModuleFactory} contractModuleFactory
      * @param {AbiCoder} abiCoder
      * @param {Accounts} accounts
