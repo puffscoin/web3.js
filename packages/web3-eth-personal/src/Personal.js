2674c9b1e263b346a633ebec25ebed53b367f61c
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-20 21:56:20 -0300

    Update Personal.js

--- packages/web3-eth-personal/src/Personal.js
+++ packages/web3-eth-personal/src/Personal.js
@@ -26,7 +26,7 @@ import {AbstractWeb3Module} from 'web3-core';
 export default class Personal extends AbstractWeb3Module {
     /**
      * TODO: Add missing documentation for getAccounts, lockAccount and sendTransaction!
-     * @param {Web3EthereumProvider|HttpProvider|WebsocketProvider|IpcProvider|String} provider
+     * @param {Web3PuffscoinProvider|HttpProvider|WebsocketProvider|IpcProvider|String} provider
      * @param {MethodFactory} methodFactory
      * @param {Network} net
      * @param {Utils} utils
