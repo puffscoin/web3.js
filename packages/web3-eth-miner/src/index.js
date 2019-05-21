45ab29af687c2ba3cd8203387ceea04c22b689fc
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-20 21:55:05 -0300

    Update index.js

--- packages/web3-eth-miner/src/index.js
+++ packages/web3-eth-miner/src/index.js
@@ -32,7 +32,7 @@ import MinerModule from './Miner.js';
  *
  * @method Miner
  *
- * @param {Web3EthereumProvider|HttpProvider|WebsocketProvider|IpcProvider|String} provider
+ * @param {Web3PuffscoinProvider|HttpProvider|WebsocketProvider|IpcProvider|String} provider
  * @param {Net.Socket} net
  * @param {Object} options
  *
