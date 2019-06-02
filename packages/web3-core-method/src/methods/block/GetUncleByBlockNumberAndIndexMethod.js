53a1835c58a73e2a06d22467506c9eee2dab019f
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-02 20:30:33 -0300

    Update GetUncleByBlockNumberAndIndexMethod.js

--- packages/web3-core-method/src/methods/block/GetUncleByBlockNumberAndIndexMethod.js
+++ packages/web3-core-method/src/methods/block/GetUncleByBlockNumberAndIndexMethod.js
@@ -31,6 +31,6 @@ export default class GetUncleByBlockNumberAndIndexMethod extends AbstractGetUncl
      * @constructor
      */
     constructor(utils, formatters, moduleInstance) {
-        super('eth_getUncleByBlockNumberAndIndex', utils, formatters, moduleInstance);
+        super('puffs_getUncleByBlockNumberAndIndex', utils, formatters, moduleInstance);
     }
 }
