7a8d27e96606161c898c58fd6700602229c02efb
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-02 20:24:20 -0300

    Update EstimateGasMethod.js

--- packages/web3-core-method/src/methods/EstimateGasMethod.js
+++ packages/web3-core-method/src/methods/EstimateGasMethod.js
@@ -31,7 +31,7 @@ export default class EstimateGasMethod extends AbstractMethod {
      * @constructor
      */
     constructor(utils, formatters, moduleInstance) {
-        super('eth_estimateGas', 1, utils, formatters, moduleInstance);
+        super('puffs_estimateGas', 1, utils, formatters, moduleInstance);
     }
 
     /**
@@ -39,7 +39,7 @@ export default class EstimateGasMethod extends AbstractMethod {
      *
      * @method beforeExecution
      *
-     * @param {AbstractWeb3Module} moduleInstance - The package where the method is called from for example Eth.
+     * @param {AbstractWeb3Module} moduleInstance - The package where the method is called from for example puffs.
      */
     beforeExecution(moduleInstance) {
         this.parameters[0] = this.formatters.inputCallFormatter(this.parameters[0], moduleInstance);
