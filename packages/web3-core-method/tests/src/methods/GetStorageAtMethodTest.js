d32e0b0fc8b12f389516a25c65b1a9615c18a751
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-03 10:19:17 -0300

    Update GetStorageAtMethodTest.js

--- packages/web3-core-method/tests/src/methods/GetStorageAtMethodTest.js
+++ packages/web3-core-method/tests/src/methods/GetStorageAtMethodTest.js
@@ -20,7 +20,7 @@ describe('GetStorageAtMethodTest', () => {
     it('constructor check', () => {
         expect(method).toBeInstanceOf(AbstractMethod);
 
-        expect(method.rpcMethod).toEqual('eth_getStorageAt');
+        expect(method.rpcMethod).toEqual('puffs_getStorageAt');
 
         expect(method.parametersAmount).toEqual(3);
 
