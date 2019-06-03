ea17853208ec35b673cc6f260e36fdb5ddfee9a9
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-03 14:28:08 -0300

    Update and rename packages/web3-eth-contract/rollup.config.js to packages/web3-puffs-contract/rollup.config.js

--- packages/web3-puffs-contract/rollup.config.js
+++ packages/web3-puffs-contract/rollup.config.js
@@ -1,4 +1,4 @@
 import pkg from './package.json';
 import rollupConfig from '../../rollup.config';
 
-export default rollupConfig('Web3EthContract', pkg.name);
+export default rollupConfig('Web3PuffsContract', pkg.name);
