0f8ae1a6cb2b69de3ca955f1032377cd572caeb5
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-03 11:09:37 -0300

    Update abstract-web3-module-test.ts

--- packages/web3-core/types/tests/abstract-web3-module-test.ts
+++ packages/web3-core/types/tests/abstract-web3-module-test.ts
@@ -79,4 +79,4 @@ abstractWeb3Module.isSameProvider('http://localhost:11363');
 abstractWeb3Module.isSameProvider(httpProvider);
 
 // $ExpectType Promise<boolean>
-abstractWeb3Module.clearSubscriptions('eth_unsubscribe');
+abstractWeb3Module.clearSubscriptions('puffs_unsubscribe');
