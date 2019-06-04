25c6e89ff3697fc954cb43c36356cf3757ac0bb7
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-04 15:38:07 -0300

    Update and rename packages/web3-eth/src/methods/GetBlockMethod.js to packages/web3-puffs/src/methods/GetBlockMethod.js

--- packages/web3-puffs/src/methods/GetBlockMethod.js
+++ packages/web3-puffs/src/methods/GetBlockMethod.js
@@ -31,7 +31,7 @@ export default class GetBlockMethod extends AbstractGetBlockMethod {
      * @constructor
      */
     constructor(utils, formatters, moduleInstance) {
-        super('eth_getBlockByNumber', utils, formatters, moduleInstance);
+        super('puffs_getBlockByNumber', utils, formatters, moduleInstance);
     }
 
     /**
@@ -43,7 +43,7 @@ export default class GetBlockMethod extends AbstractGetBlockMethod {
      */
     beforeExecution(moduleInstance) {
         if (this.isHash(this.parameters[0])) {
-            this.rpcMethod = 'eth_getBlockByHash';
+            this.rpcMethod = 'puffs_getBlockByHash';
         }
 
         super.beforeExecution(moduleInstance);
