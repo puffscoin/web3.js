ddde09d8a1aaeb7ab16c7f97bb205372b113f494
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-05 13:05:02 -0300

    Update AbiCoder.js

--- packages/web3-puffs-abi/src/AbiCoder.js
+++ packages/web3-puffs-abi/src/AbiCoder.js
@@ -32,7 +32,7 @@ export default class AbiCoder {
      *
      * @constructor
      */
-    constructor(utils, ethersAbiCoder) {
+    constructor(utils, puffssAbiCoder) {
         this.utils = utils;
         this.puffssAbiCoder = puffssAbiCoder;
     }
