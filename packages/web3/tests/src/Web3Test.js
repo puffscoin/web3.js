4c18a99eb47c40772b6700f9023002d7d9ed1f56
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-05 13:27:58 -0300

    Update Web3Test.js

--- packages/web3/tests/src/Web3Test.js
+++ packages/web3/tests/src/Web3Test.js
@@ -1,16 +1,16 @@
-import {Eth} from 'web3-eth';
+import {Puffs} from 'web3-puffs';
 import {Shh} from 'web3-shh';
 import {Network} from 'web3-net';
-import {Personal} from 'web3-eth-personal';
+import {Personal} from 'web3-puffs-personal';
 import {AbstractWeb3Module} from 'web3-core';
 import * as Utils from 'web3-utils';
 import Web3 from '../../src/Web3';
 
 // Mocks
-jest.mock('web3-eth');
+jest.mock('web3-puffs');
 jest.mock('web3-shh');
 jest.mock('web3-net');
-jest.mock('web3-eth-personal');
+jest.mock('web3-puffs-personal');
 jest.mock('web3-utils');
 
 /**
@@ -24,7 +24,7 @@ describe('Web3Test', () => {
     });
 
     it('constructor check', () => {
-        expect(web3.eth).toBeInstanceOf(Eth);
+        expect(web3.puffs).toBeInstanceOf(Puffs);
 
         expect(web3.shh).toBeInstanceOf(Shh);
 
@@ -36,7 +36,7 @@ describe('Web3Test', () => {
 
         expect(web3.defaultGasPrice).toEqual(10);
 
-        expect(Eth.mock.instances[0].defaultGasPrice).toEqual(10);
+        expect(Puffs.mock.instances[0].defaultGasPrice).toEqual(10);
 
         expect(Shh.mock.instances[0].defaultGasPrice).toEqual(10);
     });
@@ -46,7 +46,7 @@ describe('Web3Test', () => {
 
         expect(web3.defaultGas).toEqual(10);
 
-        expect(Eth.mock.instances[0].defaultGas).toEqual(10);
+        expect(Puffs.mock.instances[0].defaultGas).toEqual(10);
 
         expect(Shh.mock.instances[0].defaultGas).toEqual(10);
     });
@@ -56,7 +56,7 @@ describe('Web3Test', () => {
 
         expect(web3.transactionBlockTimeout).toEqual(10);
 
-        expect(Eth.mock.instances[0].transactionBlockTimeout).toEqual(10);
+        expect(Puffs.mock.instances[0].transactionBlockTimeout).toEqual(10);
 
         expect(Shh.mock.instances[0].transactionBlockTimeout).toEqual(10);
     });
@@ -66,7 +66,7 @@ describe('Web3Test', () => {
 
         expect(web3.transactionConfirmationBlocks).toEqual(10);
 
-        expect(Eth.mock.instances[0].transactionConfirmationBlocks).toEqual(10);
+        expect(Puffs.mock.instances[0].transactionConfirmationBlocks).toEqual(10);
 
         expect(Shh.mock.instances[0].transactionConfirmationBlocks).toEqual(10);
     });
@@ -76,7 +76,7 @@ describe('Web3Test', () => {
 
         expect(web3.transactionPollingTimeout).toEqual(10);
 
-        expect(Eth.mock.instances[0].transactionPollingTimeout).toEqual(10);
+        expect(Puffs.mock.instances[0].transactionPollingTimeout).toEqual(10);
 
         expect(Shh.mock.instances[0].transactionPollingTimeout).toEqual(10);
     });
@@ -88,7 +88,7 @@ describe('Web3Test', () => {
 
         expect(web3.defaultAccount).toEqual('0x2');
 
-        expect(Eth.mock.instances[0].defaultAccount).toEqual('0x1');
+        expect(Puffs.mock.instances[0].defaultAccount).toEqual('0x1');
 
         expect(Shh.mock.instances[0].defaultAccount).toEqual('0x1');
 
@@ -100,24 +100,24 @@ describe('Web3Test', () => {
 
         expect(web3.defaultBlock).toEqual(10);
 
-        expect(Eth.mock.instances[0].defaultBlock).toEqual(10);
+        expect(Puffs.mock.instances[0].defaultBlock).toEqual(10);
 
         expect(Shh.mock.instances[0].defaultBlock).toEqual(10);
     });
 
     it('calls setProvider and returns true', () => {
-        const ethMock = Eth.mock.instances[0];
+        const puffsMock = Puffs.mock.instances[0];
 
         const shhMock = Shh.mock.instances[0];
 
-        ethMock.setProvider = jest.fn().mockReturnValueOnce(true);
+        puffsMock.setProvider = jest.fn().mockReturnValueOnce(true);
         shhMock.setProvider = jest.fn().mockReturnValueOnce(true);
 
         expect(web3.setProvider('http://localhost', 'net')).toEqual(true);
 
         expect(web3.currentProvider.host).toEqual('http://localhost');
 
-        expect(ethMock.setProvider).toHaveBeenCalledWith('http://localhost', 'net');
+        expect(puffsMock.setProvider).toHaveBeenCalledWith('http://localhost', 'net');
 
         expect(shhMock.setProvider).toHaveBeenCalledWith('http://localhost', 'net');
     });
@@ -125,7 +125,7 @@ describe('Web3Test', () => {
     it('calls the static modules property and gets the expected object', () => {
         const modules = Web3.modules;
 
-        const eth = new modules.Eth('http://', 'net');
+        const puffs = new modules.Puffs('http://', 'net');
 
         const net = new modules.Net('http://', 'net');
 
@@ -133,7 +133,7 @@ describe('Web3Test', () => {
 
         const shh = new modules.Shh('http://', 'net');
 
-        expect(eth).toBeInstanceOf(Eth);
+        expect(puffs).toBeInstanceOf(Puffs);
 
         expect(net).toBeInstanceOf(Network);
 
