62ca57711c6efca180f9fa6d481044ac4546bfd6
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-05 13:22:19 -0300

    Update rollup.config.js

--- packages/web3/rollup.config.js
+++ packages/web3/rollup.config.js
@@ -5,8 +5,8 @@ export default rollupConfig('Web3', pkg.name, {
     'web3-core': 'web3-core',
     'web3-providers': 'web3-providers',
     'web3-utils': 'web3-utils',
-    'web3-eth': 'web3-eth',
-    'web3-eth-personal': 'web3-eth-personal',
+    'web3-puffs': 'web3-puffs',
+    'web3-puffs-personal': 'web3-puffs-personal',
     'web3-shh': 'web3-shh',
     'web3-net': 'web3-net'
 });
